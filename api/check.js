ee26fc2f22d378e444df40b2268eb102b406c3ad
josbert2 <hernandezjosbert@gmail.com>
2025-04-07 18:34:51 -0400

    Primer commit

--- api/check.js
+++ api/check.js
@@ -2,7 +2,7 @@ import { status } from 'minecraft-server-util';
 
 const IP = 'perfumaditosmod.aternos.me';
 const PORT = 52932;
-const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1358919456671273041/wy8r36DE4HkcOSMKVOplXOfMoFcHc5yRsUMj2MEsecKD-MW865uGu7-FDAN3pxxIS9Fp'; // tu webhook
+const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1358932959607918763/05KoAEu2FoPcsgvuKWbOixUr9LNsdgbraSSbzcCCk0qjWG49wDt6R_jdJH4HWQpxwDHe'; // tu webhook
 const JSONBIN_ID = '67f44e388561e97a50fa97b5'; // tu bin ID
 const JSONBIN_API_KEY = '$2a$10$IlQlYI4fYW26QU2eaybZnOyRUGsJEnrMyWdUV13WvTYykQ.Y64UKm'; // tu API Key
 
